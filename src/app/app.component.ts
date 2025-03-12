b09a1af20c4a400e7f3d514fcfb5c7301fa0a2a6
Stefan Schmidpeter <163649161+Gh0stbasta@users.noreply.github.com>
2025-03-12 11:58:27 +0000

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { LoginComponent } from './login/login.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LoginComponent, FooterComponent, HomeComponent],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
